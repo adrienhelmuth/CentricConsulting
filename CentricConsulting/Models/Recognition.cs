cb06c21ac591f0a6522df8c63053e93348cd3d4b
ah515513 <ah515513@ohio.edu>
2017-11-13 10:29:55 -0500

    Added recognition button to see other employee's recognitions

--- CentricConsulting/Models/Recognition.cs
+++ CentricConsulting/Models/Recognition.cs
@@ -22,11 +22,13 @@ namespace CentricConsulting.Models
         public DateTime recognizationDate { get; set; }
         public enum CoreValue
         {
-            Excellence = 1,
-            Integrity = 2,
-            Stewardship = 3,
-            Innovate = 4,
-            Balance = 5
+            Commit_to_Delivery_Excellence = 1,
+            Inveset_in_an_Exceptional_Culture = 2,
+            Embrace_Integrity_and_Openness = 3,
+            Practice_Responsible_Stewardship = 4,
+            Strive_to_Innovate = 5,
+            Ignite_Passion_for_the_Greater_Good = 6,
+            Live_a_Balanced_Life = 7
 
         }
     }
